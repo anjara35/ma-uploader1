647a78514a6f9ed112e7c6b27599462c9e130725
Anjara Pringault <anjaraalan@gmail.com>
2024-03-07 10:03:48 +0100

    commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Epsilon</title>
+</head>
+<body class="h-14 bg-gradient-to-b from-pink-300 to-purple-500 bg-no-repeat min-h-[100vh] ">
+    <div class="grid place-items-center my-64 bg-gradient-to-b from-white bg-no-repeat w-1/5 mx-auto rounded-xl">
+        <p class="font-bold text-[#22375c] py-5 text-3xl">EPSILON</p>
+        <p class="text-[#22375c]">Plateforme de peer-learning</p>
+        <p class="text-[#22375c] pb-5">EPSI Lille</p>
+        <img src="./img/gotaga.jfif" class="h-32 w-32 rounded-full">
+        <p class="text-[#22375c] pt-2">Anjara Pringault</p>
+        <a href="./form.php" class="underline text-purple-600 pt-2">Upload your file !</a>
+    </div>
+</body>
+</html>
